5bafd900bd69ccf45a11306faf82c8951bc52d1f
Matheus <matheusvmja@gmail.com>
2023-08-25 00:21:50 -0300

    Feat: GET/clients/:id/orders finished

--- src/controllers/clients.controller.js
+++ src/controllers/clients.controller.js
@@ -11,3 +11,38 @@ export async function createClient(req, res) {
         return res.status(500).send(err.message)
     }
 }
+
+export async function getOrdersByClient(req, res) {
+    const { id } = req.params
+
+    try {
+        const clientOrders = await db.query(`
+            SELECT 
+                 orders.id AS "orderId",
+                 orders."createdAt" AS "orderCreatedAt",
+                 orders.quantity AS "orderQuantity",
+                 orders."totalPrice" AS "orderTotalPrice",
+                 cakes.name AS "cakeName"
+            FROM orders
+            JOIN cakes ON cakes.id = orders."cakeId"
+            WHERE orders."clientId" = $1;`, [id])
+
+        if (clientOrders.rowCount === 0) return res.sendStatus(404)
+
+        const orders = clientOrders.rows.map(row => {
+            const createdAt = new Date(row.orderCreatedAt).toISOString().slice(0, 16).replace('T', ' ')
+
+            return {
+                orderId: row.orderId,
+                createdAt: createdAt,
+                quantity: row.orderQuantity,
+                totalPrice: row.orderTotalPrice,
+                cakeName: row.cakeName
+            }
+        })
+
+        res.send(orders).status(200)
+    } catch (err) {
+        return res.status(500).send(err.message)
+    }
+}
--- src/routes/clients.routes.js
+++ src/routes/clients.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express"
 import validateSchema from "../middlewares/validateSchema.middleware.js"
 import { clientsSchema } from "../schemas/clientsSchema.js"
-import { createClient } from "../controllers/clients.controller.js"
+import { createClient, getOrdersByClient } from "../controllers/clients.controller.js"
 
 const clientRouter = Router()
 
 clientRouter.post("/clients", validateSchema(clientsSchema), createClient)
+clientRouter.get("/clients/:id/orders", getOrdersByClient)
 
 export default clientRouter
